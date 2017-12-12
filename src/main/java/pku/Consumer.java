93baa0d8116bf23f64069e6bb91ac3ebad363939
chaowwwww <ccfarm@163.com>
2017-12-12 14:25:17 +0800

    version 1.0

--- src/main/java/pku/Consumer.java
+++ src/main/java/pku/Consumer.java
@@ -1,16 +1,74 @@
 package pku;
 
-import java.util.Collection;
+import java.util.*;
+import java.io.*;
 
 /**
  * Created by yangxiao on 2017/11/14.
  */
 
 public class Consumer {
-    public void attachQueue(String queueName, Collection<String> topics) throws Exception {
-
+	List<String> topics = new LinkedList<>();
+	String queue;
+	static HashMap<String, ArrayList<ByteMessage>> store = new HashMap<>();
+	static int readPos = 0;
+	static HashMap<String, Integer> readPosForMap = new HashMap<>();
+    public void attachQueue(String queueName, Collection<String> t) throws Exception {
+    	if (queue != null) {
+    		throw new Exception("This consumer could bond only one queue.");
+    	}
+    	queue = queueName;
+    	topics.addAll(t);
+    	for (Iterator<String> iter = t.iterator(); iter.hasNext(); ) {
+    		String topic = iter.next();
+    		if (!store.containsKey(topic)) {
+    			readTopic(topic);
+    		}
+    	}
+    }
+    public static synchronized void readTopic(String topic) throws Exception{
+    	store.put(topic, new ArrayList<ByteMessage>());
+		File file = new File(topic);
+		Scanner input = new Scanner(file);
+		while (input.hasNext()) {
+			int size = input.nextInt();
+			byte[] body = new byte[size];
+			for (int i = 0; i < size; i++) {
+				body[i] = input.nextByte();
+			};
+			ByteMessage msg = new DefaultMessage(body);
+			msg.putHeaders(MessageHeader.TOPIC, topic);
+			store.get(topic).add(msg);
+		}
+		input.close();
     }
     public ByteMessage poll()throws Exception{
-        return null;
+    	ByteMessage re = null;
+    	while (readPos < topics.size()) {
+    		re = pull(queue, topics.get(readPos));
+    		if (re != null) {
+    			break;
+    		}
+    	}
+        return re;
+    }
+    public static ByteMessage pull(String queue, String topic) {
+    	String k = queue + " " + topic;
+    	if (!readPosForMap.containsKey(k)) {
+    		readPosForMap.put(k, 0);
+    	}
+    	int pos = readPosForMap.get(k);
+    	if (!store.containsKey(topic)) {
+    		return null;
+    	}
+    	ArrayList<ByteMessage> list = store.get(topic);
+    	if (list.size() <= pos) {
+    		return null;
+    	} else {
+    		ByteMessage msg = list.get(pos);
+    		readPosForMap.put(k, pos + 1);
+    		return msg;
+    	}
+    	
     }
 }
--- src/main/java/pku/Producer.java
+++ src/main/java/pku/Producer.java
@@ -1,16 +1,48 @@
 package pku;
+import java.util.*;
+import java.io.*;
 
 /**
  * Created by yangxiao on 2017/11/14.
  */
 public class Producer {
+    static HashMap<String, ArrayList<ByteMessage>> store = new HashMap<>();
     public ByteMessage createBytesMessageToTopic(String topic, byte[] body)throws Exception{
-        return null;
+        ByteMessage msg = new DefaultMessage(body);
+    	msg.putHeaders(MessageHeader.TOPIC, topic);
+        return msg;
     }
     public void send(ByteMessage defaultMessage)throws Exception{
+    	String topic = defaultMessage.headers().getString(MessageHeader.TOPIC);
+    	Producer.push(defaultMessage, topic);
+    }
 
+    public static synchronized void push(ByteMessage msg, String topic) throws Exception{
+    	if (msg == null) {
+    		return;
+    	}
+    	if (!store.containsKey(topic)) {
+    		store.put(topic, new ArrayList<ByteMessage>());		
+    	}
+    	store.get(topic).add(msg);  
+    	
     }
     public void flush()throws Exception{
+    	for (Iterator iter = store.entrySet().iterator(); iter.hasNext();) {
+    		Map.Entry<String, ArrayList<ByteMessage>> entry = (Map.Entry<String, ArrayList<ByteMessage>>)iter.next();
+    		String topic = entry.getKey();
+    		ArrayList<ByteMessage> val = entry.getValue();
+    		File file = new File(topic);
+    		PrintWriter output = new PrintWriter(file);
+    		for (Iterator iter2 = val.iterator(); iter2.hasNext();) {
+    			ByteMessage msg = (DefaultMessage)iter2.next();
+    			output.print(msg.getBody().length);
+    			for (int i = 0; i < msg.getBody().length; i++) {
+    				output.print(' ' + msg.getBody()[i]);
+    			}
+    			output.println();
+    		}
+    	}
         System.out.println(1);
     }
 }
