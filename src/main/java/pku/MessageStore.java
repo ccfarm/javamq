caca51c25dc13b6db272471cd73ed9c87c8b5640
chaowwwww <ccfarm@163.com>
2017-12-18 16:15:28 +0800

    version 1.1

---
+++ src/main/java/pku/MessageStore.java
@@ -0,0 +1,141 @@
+package pku;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+
+public class MessageStore {
+	
+	static final int CAPACITY = 20 * 1024 * 1024;
+	String filename;
+	ByteBuffer buf;
+	int index;
+	
+	
+	public MessageStore(String filename) {
+		index = 0;
+		this.filename = filename;
+		buf = ByteBuffer.allocateDirect(CAPACITY);
+	}
+	
+	
+	public void putString(String st) {
+		for (int i = 0; i < st.length(); i++) {
+			buf.putChar(st.charAt(i));
+		}
+		buf.putChar('\n');
+	}
+	
+	
+	public void write() throws Exception {
+		if (buf.remaining() == CAPACITY) {
+			return;
+		}
+		RandomAccessFile rf = new RandomAccessFile("data/" + filename + index, "rw");
+		index++;
+		byte[] bytes = new byte[CAPACITY - buf.remaining()];
+		buf.get(bytes);
+		rf.write(bytes);
+		rf.close();
+		buf.clear();
+	}
+	
+	
+	public void push(ByteMessage defaultMessage) throws Exception{
+		if (defaultMessage == null) {
+			return;
+		}
+		
+		int v1 = 0;
+		long v2 = 0;
+		String v3 = null;
+		double v4 = 0;
+		v1 = defaultMessage.headers().getInt(MessageHeader.MESSAGE_ID);
+		if (v1 != 0) {
+			buf.put((byte)1);
+			buf.putInt(v1);
+		}
+		v3 = defaultMessage.headers().getString(MessageHeader.TOPIC);
+		if (v3 != null) {
+			buf.put((byte)2);
+			putString(v3);
+		}
+		v2 = defaultMessage.headers().getLong(MessageHeader.BORN_TIMESTAMP);
+		if (v2 != 0) {
+			buf.put((byte)3);
+			buf.putLong(v2);
+		}
+		v3 = defaultMessage.headers().getString(MessageHeader.BORN_HOST);
+		if (v3 != null) {
+			buf.put((byte)4);
+			putString(v3);
+		}
+		v2 = defaultMessage.headers().getLong(MessageHeader.STORE_TIMESTAMP);
+		if (v2 != 0L) {
+			buf.put((byte)5);
+			buf.putLong(v2);
+		}
+		v3 = defaultMessage.headers().getString(MessageHeader.STORE_HOST);
+		if (v3 != null) {
+			buf.put((byte)6);
+			putString(v3);
+		}
+		v2 = defaultMessage.headers().getLong(MessageHeader.START_TIME);
+		if (v2 != 0L) {
+			buf.put((byte)7);
+			buf.putLong(v2);
+		}
+		v2 = defaultMessage.headers().getLong(MessageHeader.STOP_TIME);
+		if (v2 != 0L) {
+			buf.put((byte)8);
+			buf.putLong(v2);
+		}
+		v1 = defaultMessage.headers().getInt(MessageHeader.TIMEOUT);
+		if (v1 != 0) {
+			buf.put((byte)9);
+			buf.putInt(v1);
+		}
+		v1 = defaultMessage.headers().getInt(MessageHeader.PRIORITY);
+		if (v1 != 0) {
+			buf.put((byte)10);
+			buf.putInt(v1);
+		}
+		v1 = defaultMessage.headers().getInt(MessageHeader.RELIABILITY);
+		if (v1 != 0) {
+			buf.put((byte)11);
+			buf.putInt(v1);
+		}
+		v3 = defaultMessage.headers().getString(MessageHeader.SEARCH_KEY);
+		if (v3 != null) {
+			buf.put((byte)12);
+			putString(v3);
+		}
+		v3 = defaultMessage.headers().getString(MessageHeader.SCHEDULE_EXPRESSION);
+		if (v3 != null) {
+			buf.put((byte)13);
+			putString(v3);
+		}
+		v4 = defaultMessage.headers().getDouble(MessageHeader.SHARDING_KEY);
+		if (v4 != 0.0d) {
+			buf.put((byte)14);
+			buf.putDouble(v4);
+		}
+		v4 = defaultMessage.headers().getDouble(MessageHeader.SHARDING_PARTITION);
+		if (v4 != 0.0d) {
+			buf.put((byte)15);
+			buf.putDouble(v4);
+		}
+		v3 = defaultMessage.headers().getString(MessageHeader.TRACE_ID);
+		if (v3 != null) {
+			buf.put((byte)16);
+			putString(v3);
+		}
+		buf.putInt(defaultMessage.getBody().length);
+		buf.put(defaultMessage.getBody());
+		
+		if (buf.remaining() < 201 * 1024) {
+			write();
+		}
+		
+	}
+
+}
--- src/main/java/pku/Producer.java
+++ src/main/java/pku/Producer.java
@@ -6,24 +6,26 @@ import java.io.*;
  * Created by yangxiao on 2017/11/14.
  */
 public class Producer {
+	
 	static HashMap<String, Integer> numOfTopic = new HashMap<>();
-	HashMap<String, String> nameOfFile = new HashMap<>();
+	HashMap<String, MessageStore> bufs = new HashMap<>();
     
+	
     public ByteMessage createBytesMessageToTopic(String topic, byte[] body)throws Exception{
         ByteMessage msg = new DefaultMessage(body);
     	msg.putHeaders(MessageHeader.TOPIC, topic);
         return msg;
     }
     
+    
     public void send(ByteMessage defaultMessage)throws Exception{
     	if (defaultMessage == null) {
     		return;
     	}
+    	
     	String topic = defaultMessage.headers().getString(MessageHeader.TOPIC);
     	
-    	
-    	
-    	if (!nameOfFile.containsKey(topic)) {
+    	if (!bufs.containsKey(topic)) {
     		int index = 1;
 	    	synchronized (numOfTopic) {
 				if (!numOfTopic.containsKey(topic)) {
@@ -34,109 +36,20 @@ public class Producer {
 					numOfTopic.put(topic, index);
 				}
 			}
-	    	nameOfFile.put(topic, topic + index);
+	    	bufs.put(topic, new MessageStore(index + topic));
     	}
     	
-
-    	RandomAccessFile output = new RandomAccessFile(nameOfFile.get(topic), "rw");
-    	
-    	output.seek(output.length());
-    	
-    	int v1 = 0;
-		long v2 = 0;
-		String v3 = null;
-		double v4 = 0;
-		v1 = defaultMessage.headers().getInt(MessageHeader.MESSAGE_ID);
-		if (v1 != 0) {
-			output.writeChars(MessageHeader.MESSAGE_ID + "\n");
-			output.writeInt(v1);
-			output.writeChar('\n');
-		}
-		v3 = defaultMessage.headers().getString(MessageHeader.TOPIC);
-		if (v3 != null) {
-			output.writeChars(MessageHeader.TOPIC + "\n"+ v3 + "\n");
-		}
-		v2 = defaultMessage.headers().getLong(MessageHeader.BORN_TIMESTAMP);
-		if (v2 != 0) {
-			output.writeChars(MessageHeader.BORN_TIMESTAMP + "\n");
-			output.writeLong(v2);
-			output.writeChar('\n');
-		}
-		v3 = defaultMessage.headers().getString(MessageHeader.BORN_HOST);
-		if (v3 != null) {
-			output.writeChars(MessageHeader.BORN_HOST + "\n"+ v3 + "\n");
-		}
-		v2 = defaultMessage.headers().getLong(MessageHeader.STORE_TIMESTAMP);
-		if (v2 != 0L) {
-			output.writeChars(MessageHeader.STORE_TIMESTAMP + "\n");
-			output.writeLong(v2);
-			output.writeChar('\n');
-		}
-		v3 = defaultMessage.headers().getString(MessageHeader.STORE_HOST);
-		if (v3 != null) {
-			output.writeChars(MessageHeader.STORE_HOST + "\n"+ v3 + "\n");
-		}
-		v2 = defaultMessage.headers().getLong(MessageHeader.START_TIME);
-		if (v2 != 0L) {
-			output.writeChars(MessageHeader.START_TIME + "\n");
-			output.writeLong(v2);
-			output.writeChar('\n');
-		}
-		v2 = defaultMessage.headers().getLong(MessageHeader.STOP_TIME);
-		if (v2 != 0L) {
-			output.writeChars(MessageHeader.STOP_TIME + "\n");
-			output.writeLong(v2);
-			output.writeChar('\n');
-		}
-		v1 = defaultMessage.headers().getInt(MessageHeader.TIMEOUT);
-		if (v1 != 0) {
-			output.writeChars(MessageHeader.TIMEOUT + "\n");
-			output.writeInt(v1);
-			output.writeChar('\n');
-		}
-		v1 = defaultMessage.headers().getInt(MessageHeader.PRIORITY);
-		if (v1 != 0) {
-			output.writeChars(MessageHeader.PRIORITY + "\n");
-			output.writeInt(v1);
-			output.writeChar('\n');
-		}
-		v1 = defaultMessage.headers().getInt(MessageHeader.RELIABILITY);
-		if (v1 != 0) {
-			output.writeChars(MessageHeader.RELIABILITY + "\n");
-			output.writeInt(v1);
-			output.writeChar('\n');
-		}
-		v3 = defaultMessage.headers().getString(MessageHeader.SEARCH_KEY);
-		if (v3 != null) {
-			output.writeChars(MessageHeader.SEARCH_KEY + "\n"+ v3 + "\n");
-		}
-		v3 = defaultMessage.headers().getString(MessageHeader.SCHEDULE_EXPRESSION);
-		if (v3 != null) {
-			output.writeChars(MessageHeader.SCHEDULE_EXPRESSION + "\n"+ v3 + "\n");
-		}
-		v4 = defaultMessage.headers().getDouble(MessageHeader.SHARDING_KEY);
-		if (v4 != 0.0d) {
-			output.writeChars(MessageHeader.SHARDING_KEY + "\n");
-			output.writeDouble(v4);
-			output.writeChar('\n');
-		}
-		v4 = defaultMessage.headers().getDouble(MessageHeader.SHARDING_PARTITION);
-		if (v4 != 0.0d) {
-			output.writeChars(MessageHeader.SHARDING_PARTITION + "\n");
-			output.writeDouble(v4);
-		}
-		v3 = defaultMessage.headers().getString(MessageHeader.TRACE_ID);
-		if (v3 != null) {
-			output.writeChars(MessageHeader.TRACE_ID + "\n"+ v3 + "\n");
-		}
-		output.writeChar('\r');
-		output.writeInt(defaultMessage.getBody().length);
-		output.write(defaultMessage.getBody());
-		output.close();
+    	bufs.get(topic).push(defaultMessage);
     }
     
     public void flush() throws Exception{
-    	
+    	Iterator iter = bufs.entrySet().iterator();
+    	while (iter.hasNext()) {
+    		Map.Entry<String, MessageStore> entry = (Map.Entry<String, MessageStore>) iter.next();
+    		entry.getValue().write();
+    	}
         System.out.println(1);
     }
+    
+    
 }
