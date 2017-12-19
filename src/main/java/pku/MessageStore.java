165e5cc847960059dce375307be3debe4c288c9d
chaowwwww <ccfarm@163.com>
2017-12-20 07:59:01 +0800

    更新 MessageStore.java

--- src/main/java/pku/MessageStore.java
+++ src/main/java/pku/MessageStore.java
@@ -5,7 +5,7 @@ import java.nio.ByteBuffer;
 
 public class MessageStore {
 	
-	static final int CAPACITY = 50 * 1024 * 1024;
+	static final int CAPACITY = 100 * 1024 * 1024;
 	String filename;
 	ByteBuffer buf;
 	int index;
@@ -135,7 +135,7 @@ public class MessageStore {
 		buf.putInt(defaultMessage.getBody().length);
 		buf.put(defaultMessage.getBody());
 		
-		if (buf.remaining() <= 205 * 1024) {
+		if (buf.remaining() <= 210 * 1024) {
 			write();
 		}
 		
