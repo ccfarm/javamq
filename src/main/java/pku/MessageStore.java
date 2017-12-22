3bb5cb59245b3d84da332025b2efc360f94e63cc
chaowwwww <ccfarm@163.com>
2017-12-22 20:12:41 +0800

    更新 MessageStore.java

--- src/main/java/pku/MessageStore.java
+++ src/main/java/pku/MessageStore.java
@@ -9,7 +9,7 @@ import java.io.*;
 
 public class MessageStore {
 	
-	static final int CAPACITY = 10 * 1024 * 1024;
+	static final int CAPACITY = 4 * 1024 * 1024;
 	String filename;
 	ByteBuffer buf;
 	int index;
