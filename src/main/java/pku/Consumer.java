54da70677b3996cee0112f0cda6ca6008b9fef0a
chaowwwww <ccfarm@163.com>
2017-12-23 00:40:30 +0800

    Update Consumer.java

--- src/main/java/pku/Consumer.java
+++ src/main/java/pku/Consumer.java
@@ -171,7 +171,7 @@ public class Consumer {
     	
     	
     	index++;
-    	input = new BufferedInputStream(new FileInputStream(file), 5 * 1024 * 1024);
+    	input = new BufferedInputStream(new FileInputStream(file), 10 * 1024 * 1024);
     	//System.out.println("hello world");
     	
     }
