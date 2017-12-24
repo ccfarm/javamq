423af1ccbe95e3601b2c39fa2819c627d7377e12
chaowwwww <ccfarm@163.com>
2017-12-24 08:13:10 +0800

    更新 MessageStore.java

--- src/main/java/pku/MessageStore.java
+++ src/main/java/pku/MessageStore.java
@@ -256,7 +256,7 @@ public class MessageStore {
         byte[] output = new byte[0];
 
         Deflater compresser = new Deflater();
-        compresser.setLevel(1);
+        compresser.setLevel(9);
         compresser.reset();
         compresser.setInput(data);
         compresser.finish();
