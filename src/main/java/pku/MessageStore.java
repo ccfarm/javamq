d64d1cde9e8f9e41c7e187ec7e30ac5452f28869
chaowwwww <ccfarm@163.com>
2017-12-26 22:47:54 +0800

    更新 MessageStore.java

--- src/main/java/pku/MessageStore.java
+++ src/main/java/pku/MessageStore.java
@@ -12,7 +12,7 @@ public class MessageStore {
 	static final int BUFOUTPUT = 4660 * 1024;
 	static final int BUFINPUT = 2048 * 1024;
 	static final int COMPRESS = 5 * 1024;
-	static final int DECOMPRESS = 10 * 1024;
+	static final int DECOMPRESS = 5 * 1024;
 	//String filename;
 	ByteBuffer buf;
 	RandomAccessFile output;
